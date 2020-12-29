a077b9a30ac202339f1b3a7f5d5d0a84af287c68
Austin Foss <austin.foss.account@protonmail.com>
2020-12-29 13:09:03 -0700

    Added spa mode to nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,8 @@ export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
 
+  mode: 'spa',
+
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
